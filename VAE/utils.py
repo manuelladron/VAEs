b6cccc896368d38a7f63f0719d388953c6927a27
KayEs141193 <kushal.saharan@gmail.com>
2020-07-20 17:28:18 -0400

    Added interpolate_latent

--- VAE/utils.py
+++ VAE/utils.py
@@ -24,3 +24,22 @@ def plot_results( training_hist, validation_hist ):
     plt.ylabel('nats/dim')
     plt.title('Performance on Training/Validation Set')
     plt.legend()
+
+def interpolate_latent(x1,x2,model,k=10):
+    """ Interpolates between given samples in the latent space and returns the
+    corresponding dataspace samples.
+    
+    Args:
+        x1 (tensor)
+        x2 (tensor)
+        model (model)
+        k (int): no.of interpolation b/w x1 and x2 including x1 and x2
+    Returns:
+        res (list): list of interpolated points b/w x1 and x2
+    """
+    
+    z1 = model.mapToLatent(x1.unsqueeze(0))
+    z2 = model.mapToLatent(x2.unsqueeze(0))
+
+    res = [ model.dec(torch.lerp(z1,z2,w))[0] for w in np.linspace(0,1,k,True) ]
+    return res
