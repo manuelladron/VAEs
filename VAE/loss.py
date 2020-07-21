3cab07dd71fe3dfa144d949611e62688060cbaf8
KayEs141193 <kushal.saharan@gmail.com>
2020-07-21 07:32:19 -0400

    Removing constant for better resolution of optimization

--- VAE/loss.py
+++ VAE/loss.py
@@ -11,11 +11,12 @@ def kl_loss(mu_z,logstd_z):
 
 def recon_loss(x,x_recon,logstd_noise,device):
     
-    l1 = torch.log(torch.tensor(2*np.pi,device=device))
+    # Removing constant term for better resolution of the optimization
+    #l1 = torch.log(torch.tensor(2*np.pi,device=device))
     l2 = 2*logstd_noise
     l3 = ((x-x_recon)**2/torch.exp(2*logstd_noise))
 
-    return 0.5*(l1+l2+l3).sum(1).mean()
+    return 0.5*(l2+l3).sum(1).mean()
 
 def elbo_loss(x,x_recon,logstd_noise,mu_z,logstd_z,device):
     
