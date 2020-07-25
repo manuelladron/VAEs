e1cf4e5f227c58986265a443fdeb608b4e4539f1
KayEs141193 <kushal.saharan@gmail.com>
2020-07-25 15:56:35 -0400

    Added Bernoulli recon loss and makeLossLayered

--- VAE/loss.py
+++ VAE/loss.py
@@ -1,4 +1,5 @@
 import torch
+from torch.distributions import bernoulli
 import numpy as np
 
 def kl_loss(mu_z,logstd_z):
@@ -9,7 +10,7 @@ def kl_loss(mu_z,logstd_z):
 
     return 0.5*(l1+l2-1-l3).sum(1).mean()
 
-def recon_loss(x,x_recon,logstd_noise,device):
+def recon_loss(x,x_recon,logstd_noise):
     
     # Removing constant term for better resolution of the optimization
     #l1 = torch.log(torch.tensor(2*np.pi,device=device))
@@ -24,3 +25,15 @@ def elbo_loss(x,x_recon,logstd_noise,mu_z,logstd_z,device):
     recon = recon_loss(x,x_recon,logstd_noise,device)
 
     return kl + recon, kl, recon
+
+def recon_loss_bernoulli(x,logits,*args,**kwargs):
+    # *args to take in extra variables to fit in the existing trainer setup
+    rv = bernoulli.Bernoulli(logits=logits)
+    return -rv.log_prob(x).sum(1).mean()
+
+def makeLossLayered(loss):
+    
+    def func(*args):
+       return sum([  loss(*arg) for arg in zip(*args) ])
+
+    return func
