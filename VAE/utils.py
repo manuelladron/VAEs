9d9a56784de9be4c1521b56ea1bd42667d120c13
KayEs141193 <kushal.saharan@gmail.com>
2020-07-25 15:58:01 -0400

    Added supress parameter for print_and_collect

--- VAE/utils.py
+++ VAE/utils.py
@@ -2,15 +2,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch
 
-def print_and_collect(engine, evaluator, dataloader, mode, history_dict):
+def print_and_collect(engine, evaluator, dataloader, mode, history_dict,supress=False):
     
     evaluator.run(dataloader, max_epochs=1)
     metrics = evaluator.state.metrics
-
-    print(
-        mode + " Results - Epoch {} - ELBO loss: {:.2f} RECON loss: {:.2f} KL loss: {:.2f}"
-        .format(engine.state.epoch, metrics['elbo_loss'], metrics['recon_loss'], 
-        metrics['kl_loss']))
+    
+    if not supress:
+        print(
+            mode + " Results - Epoch {} - ELBO loss: {:.2f} RECON loss: {:.2f} KL loss: {:.2f}"
+            .format(engine.state.epoch, metrics['elbo_loss'], metrics['recon_loss'], 
+            metrics['kl_loss']))
 
     for key in history_dict.keys():
         history_dict[key].append(metrics[key])
